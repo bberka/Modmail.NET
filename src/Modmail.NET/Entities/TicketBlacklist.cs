685db190f046647b08e1bcf96635296c85ad53d1
bberka <berkaybyr@icloud.com>
2024-03-19 21:00:50 +0300

    feat: TicketBlacklist.cs entity added

---
+++ src/Modmail.NET/Entities/TicketBlacklist.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modmail.NET.Entities;
+
+public class TicketBlacklist
+{
+  [Key]
+  public Guid Id { get; set; }
+  
+  public DateTime RegisterDateUtc { get; set; } = DateTime.UtcNow;
+  
+  public string? Reason { get; set; }
+  
+  public ulong DiscordUserInfoId { get; set; }
+  
+  public int BlacklistForMinutes { get; set; }  
+  
+  //FK
+
+  public virtual DiscordUserInfo DiscordUserInfo { get; set; }
+  
+  
+  public DateTime GetEndDateUtc() => RegisterDateUtc.AddMinutes(BlacklistForMinutes);
+}
