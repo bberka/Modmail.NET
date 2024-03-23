f9edbfc41a41c431511eb38aae60c7f21fad6160
bberka <berkaybyr@icloud.com>
2024-03-23 15:39:33 +0300

    fix: removed color code prop from ticket type cleaned up property

--- src/Modmail.NET/Entities/TicketType.cs
+++ src/Modmail.NET/Entities/TicketType.cs
@@ -7,16 +7,11 @@ public class TicketType
   [Key]
   public Guid Id { get; set; }
 
-  public bool IsEnabled { get; set; } = true;
   public DateTime RegisterDateUtc { get; set; } = DateTime.UtcNow;
   public DateTime? UpdateDateUtc { get; set; }
   public string Key { get; set; }
   public string Name { get; set; }
   public string? Emoji { get; set; }
-
-  // public string? ColorHexCode { get; set; }
   public string? Description { get; set; }
-
   public int Order { get; set; }
-  // public ulong? CategoryId { get; set; }
 }
