6d494f4d84601774ed431fbf6d53cb9a847bdcaf
bberka <berkaybyr@icloud.com>
2024-03-26 03:17:18 +0300

    feat: added small caching for auto complete providers

--- src/Modmail.NET/Providers/TeamProvider.cs
+++ src/Modmail.NET/Providers/TeamProvider.cs
@@ -1,11 +1,13 @@
 ﻿using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
+using Modmail.NET.Aspects;
 using Modmail.NET.Entities;
 
 namespace Modmail.NET.Providers;
 
 public class TeamProvider : IAutocompleteProvider
 {
+  [CacheAspect(CacheSeconds = 3)]
   public async Task<IEnumerable<DiscordAutoCompleteChoice>> Provider(AutocompleteContext ctx) {
     var teamsDbList = await GuildTeam.GetAllAsync(ctx.Guild.Id);
     var teams = teamsDbList.Select(x => new DiscordAutoCompleteChoice(x.Name, x.Name));
--- src/Modmail.NET/Providers/TicketTypeProvider.cs
+++ src/Modmail.NET/Providers/TicketTypeProvider.cs
@@ -1,11 +1,13 @@
 ﻿using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
+using Modmail.NET.Aspects;
 using Modmail.NET.Entities;
 
 namespace Modmail.NET.Providers;
 
 public class TicketTypeProvider : IAutocompleteProvider
 {
+  [CacheAspect(CacheSeconds = 3)]
   public async Task<IEnumerable<DiscordAutoCompleteChoice>> Provider(AutocompleteContext ctx) {
     var ticketTypesDbList = await TicketType.GetAllAsync();
     var ticketTypes = ticketTypesDbList.Select(x => new DiscordAutoCompleteChoice(x.Name, x.Name));
