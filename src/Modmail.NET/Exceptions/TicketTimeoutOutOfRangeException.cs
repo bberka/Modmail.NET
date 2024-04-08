7e02f670324030faf83b0b36ebd9e477aa1eceb4
bberka <berkaybyr@icloud.com>
2024-04-08 14:41:50 +0300

    added TicketTimeoutOutOfRangeException.cs exception

---
+++ src/Modmail.NET/Exceptions/TicketTimeoutOutOfRangeException.cs
@@ -0,0 +1,8 @@
+﻿namespace Modmail.NET.Exceptions;
+
+public class TicketTimeoutOutOfRangeException : BotExceptionBase
+{
+  public TicketTimeoutOutOfRangeException() : base(LangKeys.TICKET_TIMEOUT_VALUE_IS_OUT_OF_RANGE.GetTranslation(),
+                                                   LangKeys.TICKET_TIMEOUT_VALUE_MUST_BE_BETWEEN_X_AND_Y.GetTranslation(Const.TICKET_TIMEOUT_MIN_ALLOWED_HOURS,
+                                                                                                                        Const.TICKET_TIMEOUT_MAX_ALLOWED_HOURS)) { }
+}
