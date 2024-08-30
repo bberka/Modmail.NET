01368c552dec37c204d9e868d17a3e4737f4faf2
Berkay <berkaybyr@icloud.com>
2024-08-30 12:09:15 +0300

    removed unused message template

--- src/Modmail.NET/Common/CommandResponses.cs
+++ src/Modmail.NET/Common/CommandResponses.cs
@@ -27,36 +27,4 @@ public static class CommandResponses
 
     return new DiscordWebhookBuilder().AddEmbed(embed);
   }
-
-  public static DiscordWebhookBuilder ListTeams(DiscordGuild guild, List<GuildTeam> teams) {
-    var embed = new DiscordEmbedBuilder()
-                .WithTitle(LangKeys.TEAM_LIST.GetTranslation())
-                .WithColor(Colors.InfoColor);
-
-    foreach (var team in teams) {
-      var sb = new StringBuilder();
-      sb.AppendLine($"`{LangKeys.ENABLED}`: {team.IsEnabled}");
-      sb.AppendLine($"`{LangKeys.PERMISSION_LEVEL}`: {team.PermissionLevel}");
-      sb.AppendLine($"`{LangKeys.MEMBERS}`: {team.GuildTeamMembers.Count}");
-      sb.AppendLine($"`{LangKeys.PING_ON_NEW_TICKET}`: {team.PingOnNewTicket}");
-      sb.AppendLine($"`{LangKeys.PING_ON_NEW_MESSAGE}`: {team.PingOnNewMessage}");
-      foreach (var member in team.GuildTeamMembers.OrderBy(x => x.Type))
-        switch (member.Type) {
-          case TeamMemberDataType.RoleId:
-            //Tag role 
-            sb.AppendLine($"`{LangKeys.ROLE}`: <@&{member.Key}>");
-            break;
-          case TeamMemberDataType.UserId:
-            //tag member
-            sb.AppendLine($"`{LangKeys.USER}`: <@{member.Key}>");
-            break;
-          default:
-            throw new ArgumentOutOfRangeException();
-        }
-
-      embed.AddField(team.Name, sb.ToString());
-    }
-
-    return new DiscordWebhookBuilder().AddEmbed(embed);
-  }
 }
