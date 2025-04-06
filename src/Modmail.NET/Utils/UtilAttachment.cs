9dc899b23e4d5f0a01b36824f9ef0ae5305e8360
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-06 17:04:24 +0300

    feat: UtilAttachment.cs
    - Returns attachment uri path from config with attachment id

---
+++ src/Modmail.NET/Utils/UtilAttachment.cs
@@ -0,0 +1,12 @@
+namespace Modmail.NET.Utils;
+
+public static class UtilAttachment
+{
+  public static string GetUri(Guid id) {
+    var botConfig = ServiceLocator.GetBotConfig();
+    if (string.IsNullOrEmpty(botConfig.Domain)) throw new InvalidOperationException();
+
+    var uri = new Uri(botConfig.Domain, UriKind.Absolute);
+    return uri + "/attachments/" + id;
+  }
+}
