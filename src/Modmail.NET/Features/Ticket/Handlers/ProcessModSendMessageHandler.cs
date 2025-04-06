833a4edeae4f68e7a91f5e1b9b6e6f4b36cbeca8
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-06 19:13:03 +0300

    feat: Handle attachment uploads in ticket messages
    This commit introduces full attachment support for Modmail tickets:
    
    -   Attachments are now downloaded to a local directory (defined in `Const.cs`).
    -   Attachment downloads are now handled directly, removing the queue.
    -   Instead of sending copied embeds, the bot now reacts to messages after processing.
    -   `BotMessageIds` are correctly saved to the `TicketMessage` table.
    -   `TicketAttachmentDownloadService` is used via DI.
    -   Fixed an issue where `BotTicketCreatedMessageInDmId` was not saved correctly.
    -   The bot now awaits message handling tasks to ensure correct processing order, especially for attachment ID and download requirements.

--- src/Modmail.NET/Features/Ticket/Handlers/ProcessModSendMessageHandler.cs
+++ src/Modmail.NET/Features/Ticket/Handlers/ProcessModSendMessageHandler.cs
@@ -4,23 +4,26 @@ using Modmail.NET.Database;
 using Modmail.NET.Entities;
 using Modmail.NET.Exceptions;
 using Modmail.NET.Features.Guild;
-using Modmail.NET.Queues;
+using Modmail.NET.Services;
 using Modmail.NET.Utils;
 
 namespace Modmail.NET.Features.Ticket.Handlers;
 
 public class ProcessModSendMessageHandler : IRequestHandler<ProcessModSendMessageCommand>
 {
+  private readonly TicketAttachmentDownloadService _attachmentDownloadService;
   private readonly ModmailBot _bot;
   private readonly ModmailDbContext _dbContext;
   private readonly ISender _sender;
 
   public ProcessModSendMessageHandler(ISender sender,
                                       ModmailBot bot,
-                                      ModmailDbContext dbContext) {
+                                      ModmailDbContext dbContext,
+                                      TicketAttachmentDownloadService attachmentDownloadService) {
     _sender = sender;
     _bot = bot;
     _dbContext = dbContext;
+    _attachmentDownloadService = attachmentDownloadService;
   }
 
   public async Task Handle(ProcessModSendMessageCommand request, CancellationToken cancellationToken) {
@@ -29,32 +32,31 @@ public class ProcessModSendMessageHandler : IRequestHandler<ProcessModSendMessag
     ArgumentNullException.ThrowIfNull(request.Channel);
     ArgumentNullException.ThrowIfNull(request.Guild);
 
-    var ticketMessage = TicketMessage.MapFrom(request.TicketId, request.Message, true);
 
     var ticket = await _sender.Send(new GetTicketQuery(request.TicketId, MustBeOpen: true), cancellationToken);
     ticket.LastMessageDateUtc = UtilDate.GetNow();
 
     _dbContext.Update(ticket);
+
+
+    var guildOption = await _sender.Send(new GetGuildOptionQuery(false), cancellationToken);
+
+    var ticketMessage = TicketMessage.MapFrom(request.TicketId, request.Message, true);
+
+    foreach (var attachment in ticketMessage.Attachments)
+      await _attachmentDownloadService.Handle(attachment.Id, attachment.Url, Path.GetExtension(attachment.FileName));
+
+    var anonymous = guildOption.AlwaysAnonymous || ticket.Anonymous;
+    var privateChannel = await _bot.Client.GetChannelAsync(ticket.PrivateMessageChannelId);
+    var embed = UserResponses.MessageReceived(request.Message, ticketMessage.Attachments.ToArray(), anonymous);
+    var botMessage = await privateChannel.SendMessageAsync(embed);
+
+    ticketMessage.BotMessageId = botMessage.Id;
     await _dbContext.AddAsync(ticketMessage, cancellationToken);
 
     var affected = await _dbContext.SaveChangesAsync(cancellationToken);
     if (affected == 0) throw new DbInternalException();
 
-    var guildOption = await _sender.Send(new GetGuildOptionQuery(false), cancellationToken);
-
-
-    foreach (var attachment in ticketMessage.Attachments)
-      TicketAttachmentDownloadQueueHandler.Enqueue(attachment.Id, attachment.Url, Path.GetExtension(attachment.FileName));
-
-
-    _ = Task.Run(async () => {
-      var anonymous = guildOption.AlwaysAnonymous || ticket.Anonymous;
-      var privateChannel = await _bot.Client.GetChannelAsync(ticket.PrivateMessageChannelId);
-      var embed = UserResponses.MessageReceived(request.Message, ticketMessage.Attachments.ToArray(), anonymous);
-      await privateChannel.SendMessageAsync(embed);
-
-      var bot = ServiceLocator.GetModmailBot();
-      await request.Message.CreateReactionAsync(DiscordEmoji.FromName(bot.Client, Const.ProcessedReactionDiscordEmojiString, false));
-    }, cancellationToken);
+    await request.Message.CreateReactionAsync(DiscordEmoji.FromName(_bot.Client, Const.ProcessedReactionDiscordEmojiString, false));
   }
 }
