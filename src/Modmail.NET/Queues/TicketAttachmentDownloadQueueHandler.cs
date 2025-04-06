a8dcc0eefdfb90720055a80960992cb05b4267cd
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-06 16:29:56 +0300

    feat(TicketAttachmentDownloadQueueHandler.cs): improved log messages, added Enqueue method

--- src/Modmail.NET/Queues/TicketAttachmentDownloadQueueHandler.cs
+++ src/Modmail.NET/Queues/TicketAttachmentDownloadQueueHandler.cs
@@ -1,13 +1,18 @@
+using Hangfire;
 using Serilog;
 
 namespace Modmail.NET.Queues;
 
 public static class TicketAttachmentDownloadQueueHandler
 {
+  public static void Enqueue(Guid attachmentId, string url, string fileExtension) {
+    BackgroundJob.Enqueue(HangfireQueueName.AttachmentDownload.Name, () => Handle(attachmentId, url, fileExtension));
+  }
+
   public static async Task Handle(Guid attachmentId, string url, string fileExtension) {
     if (!Directory.Exists(Const.AttachmentDownloadDirectory)) Directory.CreateDirectory(Const.AttachmentDownloadDirectory);
 
-    var filePath = Path.Combine(Const.AttachmentDownloadDirectory, $"{attachmentId}.{fileExtension}");
+    var filePath = Path.Combine(Const.AttachmentDownloadDirectory, $"{attachmentId}.{fileExtension.Trim().Trim('.')}");
     if (Path.Exists(filePath)) throw new InvalidOperationException("File in path already exists: " + filePath);
 
     var httpClient = ServiceLocator.CreateHttpClient();
@@ -23,13 +28,13 @@ public static class TicketAttachmentDownloadQueueHandler
         }
       }
 
-      Log.Information("File downloaded successfully to {FilePath}", filePath);
+      Log.Information("[TicketAttachmentDownloadQueueHandler] File downloaded successfully to {FilePath}", filePath);
     }
     catch (HttpRequestException ex) {
-      Log.Error(ex, "Error downloading file from {Url}: {ExMessage}", url, ex.Message);
+      Log.Error(ex, "[TicketAttachmentDownloadQueueHandler] Error downloading file from {Url}: {ExMessage}", url, ex.Message);
     }
     catch (Exception ex) {
-      Log.Fatal(ex, "An exception occurred: {ExMessage}", ex.Message);
+      Log.Fatal(ex, "[TicketAttachmentDownloadQueueHandler] An exception occurred: {ExMessage}", ex.Message);
     }
     finally {
       httpClient?.Dispose();
