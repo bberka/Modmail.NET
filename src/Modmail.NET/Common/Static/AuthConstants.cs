53a4c11ea1c1de81233ae1814b92fbb3ea907eb9
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-14 17:22:43 +0300

    feat: AuthConstants.cs
    - PermissionsClaimType
    - AuthorizeTeamRole

---
+++ src/Modmail.NET/Common/Static/AuthConstants.cs
@@ -0,0 +1,7 @@
+namespace Modmail.NET.Features.Teams.Static;
+
+public static class AuthConstants
+{
+  public const string PermissionsClaimType = "Claim.Modmail.NET.Permissions";
+  public const string AuthorizeTeamRole = "Role.Modmail.NET.Permissions";
+}
