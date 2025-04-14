b02d84fb6f386777f519dc9298a49a98c7ee3880
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-14 17:47:39 +0300

    fix(UtilMention.cs): ping user filter
    split existing method into 2 new:
    - GetNewTicketPingText
    - GetNewMessagePingText

--- src/Modmail.NET/Common/Utils/UtilMention.cs
+++ src/Modmail.NET/Common/Utils/UtilMention.cs
@@ -1,14 +1,20 @@
 using System.Text;
-using Modmail.NET.Features.Permission.Models;
+using Modmail.NET.Features.Teams.Models;
 
 namespace Modmail.NET.Common.Utils;
 
 public static class UtilMention
 {
-  public static string GetMentionsMessageString(IEnumerable<PermissionInfo> permissions) {
+  //TODO: Check if ping notification works for the team users
+  public static string GetNewTicketPingText(UserTeamInformation[] users) {
     var sb = new StringBuilder();
-    foreach (var perm in permissions) sb.AppendLine(perm.GetMention());
+    foreach (var perm in users.Where(x => x.PingOnNewTicket)) sb.AppendLine(perm.GetMention());
+    return sb.ToString();
+  }
 
+  public static string GetNewMessagePingText(UserTeamInformation[] users) {
+    var sb = new StringBuilder();
+    foreach (var perm in users.Where(x => x.PingOnNewMessage)) sb.AppendLine(perm.GetMention());
     return sb.ToString();
   }
 }
