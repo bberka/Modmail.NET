f9aefe5d4fdf8699f1b168a0531f4c8723d98284
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-14 17:19:30 +0300

    feat: CachingPipelineBehavior.cs now passes cancellation token

--- src/Modmail.NET/Pipeline/CachingPipelineBehavior.cs
+++ src/Modmail.NET/Pipeline/CachingPipelineBehavior.cs
@@ -18,16 +18,16 @@ public class CachingPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TR
 
   public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken) {
     var cacheAttribute = typeof(TRequest).GetCustomAttribute<CachePolicyAttribute>();
-    if (cacheAttribute == null) return await next();
+    if (cacheAttribute == null) return await next(cancellationToken);
     var cacheKey = UtilCache.BuildCacheKeyFromT(cacheAttribute.CacheKey, cacheAttribute.IncludeRequestDataToCacheKey
                                                                            ? request
                                                                            : default);
-    if (_cache.TryGetValue(cacheKey, out TResponse cachedResponse) && cachedResponse is not null) {
+    if (_cache.TryGetValue(cacheKey, out TResponse? cachedResponse) && cachedResponse is not null) {
       Log.Verbose("Returning cached data for: {CacheKey}", cacheKey);
       return cachedResponse;
     }
 
-    var response = await next();
+    var response = await next(cancellationToken);
     _cache.Set(cacheKey, response, TimeSpan.FromSeconds(cacheAttribute.DurationInSeconds));
     Log.Verbose("Caching response for: {CacheKey} (Duration: {CacheAttributeDurationInSeconds}s)", cacheKey, cacheAttribute.DurationInSeconds);
     return response;
