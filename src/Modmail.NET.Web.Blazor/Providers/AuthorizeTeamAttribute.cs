694252dac70304e0c6238106c24ed52386c9810b
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-14 19:09:52 +0300

    feat: implement custom role apply on auth attribute

--- src/Modmail.NET.Web.Blazor/Providers/AuthorizeTeamAttribute.cs
+++ src/Modmail.NET.Web.Blazor/Providers/AuthorizeTeamAttribute.cs
@@ -1,20 +1,21 @@
 using Microsoft.AspNetCore.Authorization;
 using Modmail.NET.Common.Static;
-using Modmail.NET.Features.Permission.Static;
 
 namespace Modmail.NET.Web.Blazor.Providers;
 
 public class AuthorizeTeamAttribute : AuthorizeAttribute
 {
   public AuthorizeTeamAttribute() {
-    Roles = string.Join(",", Enum.GetNames<TeamPermissionLevel>());
+    Roles = AuthConstants.AuthorizeTeamRole;
   }
 
   public AuthorizeTeamAttribute(AuthPolicy policy) {
+    Roles = AuthConstants.AuthorizeTeamRole;
     Policy = policy.Name;
   }
 
   public AuthorizeTeamAttribute(string policy) {
+    Roles = AuthConstants.AuthorizeTeamRole;
     Policy = policy;
   }
 }
