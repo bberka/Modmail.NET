942e98d7f8cce9006f3bb29220ef2edca9d7eea8
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-14 19:09:34 +0300

    feat(WebExtensions.cs): implement custom role check

--- src/Modmail.NET.Web.Blazor/Extensions/WebExtensions.cs
+++ src/Modmail.NET.Web.Blazor/Extensions/WebExtensions.cs
@@ -1,4 +1,7 @@
 using System.Security.Claims;
+using Modmail.NET.Common.Exceptions;
+using Modmail.NET.Common.Static;
+using Modmail.NET.Language;
 
 namespace Modmail.NET.Web.Blazor.Extensions;
 
@@ -11,6 +14,9 @@ public static class WebExtensions
     var val = ulong.Parse(nameId);
 
     if (val == 0) throw new Exception("UserId does not exist in claims");
+
+    var role = principal.FindFirst(ClaimTypes.Role)?.Value;
+    if (role != AuthConstants.AuthorizeTeamRole) throw new ModmailBotException(Lang.UnauthorizedAccess);
 
     return val;
   }
