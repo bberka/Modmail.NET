5deb24073e29efc7e61cfd7f019812b5da16d973
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-16 00:02:42 +0300

    fix: CheckTeamExistsQuery permission check interface removed

--- src/Modmail.NET/Features/Teams/Queries/CheckTeamExistsQuery.cs
+++ src/Modmail.NET/Features/Teams/Queries/CheckTeamExistsQuery.cs
@@ -1,6 +1,3 @@
-using Modmail.NET.Abstract;
-
 namespace Modmail.NET.Features.Teams.Queries;
 
-public sealed record CheckTeamExistsQuery(ulong AuthorizedUserId, string Name) : IRequest<bool>,
-                                                                                 IPermissionCheck;
+public sealed record CheckTeamExistsQuery(ulong AuthorizedUserId, string Name) : IRequest<bool>;
