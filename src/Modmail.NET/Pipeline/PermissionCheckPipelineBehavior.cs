47a4eb9f413d8a31ed8c049d1829931f2e6769a5
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-15 21:50:06 +0300

    feat(PermissionCheckPipelineBehavior.cs): implement AuthorizedUserId validation

--- src/Modmail.NET/Pipeline/PermissionCheckPipelineBehavior.cs
+++ src/Modmail.NET/Pipeline/PermissionCheckPipelineBehavior.cs
@@ -9,27 +9,28 @@ using Modmail.NET.Language;
 namespace Modmail.NET.Pipeline;
 
 public class PermissionCheckPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-  where TRequest : IPermissionCheck, IRequest<TResponse>
+	where TRequest : IPermissionCheck, IRequest<TResponse>
 {
-  private readonly ISender _sender;
+	private readonly ISender _sender;
 
-  public PermissionCheckPipelineBehavior(ISender sender) {
-    _sender = sender;
-  }
+	public PermissionCheckPipelineBehavior(ISender sender) {
+		_sender = sender;
+	}
 
-  public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken) {
-    var attribute = typeof(TRequest).GetCustomAttribute<RequireModmailPermissionAttribute>();
-    if (attribute == null) throw new InvalidOperationException();
+	public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken) {
+		if (request.AuthorizedUserId <= 0) throw new ModmailBotException(Lang.UnauthorizedAccess);
+		var attribute = typeof(TRequest).GetCustomAttribute<RequireModmailPermissionAttribute>();
+		if (attribute == null) throw new InvalidOperationException();
 
-    if (attribute.AuthPolicy is null) {
-      var isAnyTeamMember = await _sender.Send(new CheckUserInAnyTeamQuery(request.AuthorizedUserId), cancellationToken);
-      if (!isAnyTeamMember) throw new ModmailBotException(Lang.UnauthorizedAccess);
-      return await next(cancellationToken);
-    }
+		if (attribute.AuthPolicy is null) {
+			var isAnyTeamMember = await _sender.Send(new CheckUserInAnyTeamQuery(request.AuthorizedUserId), cancellationToken);
+			if (!isAnyTeamMember) throw new ModmailBotException(Lang.UnauthorizedAccess);
+			return await next(cancellationToken);
+		}
 
-    var allowed = await _sender.Send(new CheckPermissionAccessQuery(request.AuthorizedUserId, attribute.AuthPolicy), cancellationToken);
-    if (!allowed) throw new ModmailBotException(Lang.UnauthorizedAccess);
+		var allowed = await _sender.Send(new CheckPermissionAccessQuery(request.AuthorizedUserId, attribute.AuthPolicy), cancellationToken);
+		if (!allowed) throw new ModmailBotException(Lang.UnauthorizedAccess);
 
-    return await next(cancellationToken);
-  }
+		return await next(cancellationToken);
+	}
 }
