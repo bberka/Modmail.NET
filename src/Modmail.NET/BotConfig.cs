32e09f4ee3206309b825a8685eef8d3061beeebf
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-16 02:38:55 +0300

    feat: domain uri parse on init

--- src/Modmail.NET/BotConfig.cs
+++ src/Modmail.NET/BotConfig.cs
@@ -5,17 +5,25 @@ namespace Modmail.NET;
 
 public class BotConfig
 {
-  public EnvironmentType Environment { get; set; }
-  public required string BotToken { get; set; }
-  public required string BotClientId { get; set; }
-  public required string BotClientSecret { get; set; }
-  public required string BotPrefix { get; set; }
-  public ulong MainServerId { get; set; }
-  public required ulong[] SuperUsers { get; set; }
-  public required string DbConnectionString { get; set; }
-  public required string DefaultLanguage { get; set; }
-  public bool SensitiveEfCoreDataLog { get; set; }
+	private readonly Uri? _domain;
+	public EnvironmentType Environment { get; init; }
+	public required string BotToken { get; init; }
+	public required string BotClientId { get; init; }
+	public required string BotClientSecret { get; init; }
+	public required string BotPrefix { get; init; }
+	public ulong MainServerId { get; init; }
+	public required ulong[] SuperUsers { get; init; }
+	public required string DbConnectionString { get; init; }
+	public required string DefaultLanguage { get; init; }
+	public bool SensitiveEfCoreDataLog { get; init; }
 
-  [Url]
-  public required string Domain { get; set; }
+	[Url]
+	public required string Domain {
+		get => _domain?.AbsoluteUri ?? throw new InvalidOperationException("Domain is not set");
+		init => _domain = Uri.TryCreate(value, UriKind.Absolute, out var uri)
+			                  ? uri
+			                  : throw new ArgumentException("Invalid URL", nameof(value));
+	}
+
+	public Uri DomainUri => _domain ?? throw new InvalidOperationException("Domain is not set");
 }
