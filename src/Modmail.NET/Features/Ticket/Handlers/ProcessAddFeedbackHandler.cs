2be72be7a326234c43c614fa8e49ce2c1bd61795
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-15 18:32:04 +0300

    chore: removed unnecessary comment

--- src/Modmail.NET/Features/Ticket/Handlers/ProcessAddFeedbackHandler.cs
+++ src/Modmail.NET/Features/Ticket/Handlers/ProcessAddFeedbackHandler.cs
@@ -10,38 +10,37 @@ namespace Modmail.NET.Features.Ticket.Handlers;
 
 public class ProcessAddFeedbackHandler : IRequestHandler<ProcessAddFeedbackCommand>
 {
-  private readonly ModmailDbContext _dbContext;
-  private readonly ISender _sender;
+	private readonly ModmailDbContext _dbContext;
+	private readonly ISender _sender;
 
-  public ProcessAddFeedbackHandler(ISender sender,
-                                   ModmailDbContext dbContext,
-                                   ModmailBot bot) {
-    _sender = sender;
-    _dbContext = dbContext;
-  }
+	public ProcessAddFeedbackHandler(ISender sender,
+	                                 ModmailDbContext dbContext,
+	                                 ModmailBot bot) {
+		_sender = sender;
+		_dbContext = dbContext;
+	}
 
-  public async Task Handle(ProcessAddFeedbackCommand request, CancellationToken cancellationToken) {
-    if (request.StarCount is < 1 or > 5) throw new InvalidOperationException("Star count must be between 1 and 5");
-    if (string.IsNullOrEmpty(request.TextInput)) throw new InvalidOperationException("Feedback messageContent is empty");
-    if (request.FeedbackMessage is null) throw new InvalidOperationException("Feedback messageContent is null");
+	public async Task Handle(ProcessAddFeedbackCommand request, CancellationToken cancellationToken) {
+		if (request.StarCount is < 1 or > 5) throw new InvalidOperationException("Star count must be between 1 and 5");
+		if (string.IsNullOrEmpty(request.TextInput)) throw new InvalidOperationException("Feedback messageContent is empty");
+		if (request.FeedbackMessage is null) throw new InvalidOperationException("Feedback messageContent is null");
 
-    var guildOption = await _sender.Send(new GetOptionQuery(), cancellationToken);
-    if (!guildOption.TakeFeedbackAfterClosing) throw new InvalidOperationException("Feedback is not enabled for this guild: " + guildOption.ServerId);
+		var guildOption = await _sender.Send(new GetOptionQuery(), cancellationToken);
+		if (!guildOption.TakeFeedbackAfterClosing) throw new InvalidOperationException("Feedback is not enabled for this guild: " + guildOption.ServerId);
 
-    var ticket = await _dbContext.Tickets.FindAsync([request.TicketId], cancellationToken) ?? throw new NullReferenceException(nameof(Ticket));
-    ticket.ThrowIfNotClosed();
-    if (ticket.FeedbackStar.HasValue) throw new ModmailBotException(Lang.FeedbackAlreadySubmitted);
+		var ticket = await _dbContext.Tickets.FindAsync([request.TicketId], cancellationToken) ?? throw new NullReferenceException(nameof(Ticket));
+		ticket.ThrowIfNotClosed();
+		if (ticket.FeedbackStar.HasValue) throw new ModmailBotException(Lang.FeedbackAlreadySubmitted);
 
-    ticket.FeedbackStar = request.StarCount;
-    ticket.FeedbackMessage = request.TextInput;
-    _dbContext.Update(ticket);
-    var affected = await _dbContext.SaveChangesAsync(cancellationToken);
-    if (affected == 0) throw new DbInternalException();
+		ticket.FeedbackStar = request.StarCount;
+		ticket.FeedbackMessage = request.TextInput;
+		_dbContext.Update(ticket);
+		var affected = await _dbContext.SaveChangesAsync(cancellationToken);
+		if (affected == 0) throw new DbInternalException();
 
-    await request.FeedbackMessage.ModifyAsync(x => {
-      x.Clear();
-      x.AddEmbed(TicketBotMessages.User.FeedbackReceivedUpdateMessage(ticket));
-    });
-    // _ = Task.Run(async () => {  }); }, cancellationToken);
-  }
+		await request.FeedbackMessage.ModifyAsync(x => {
+			x.Clear();
+			x.AddEmbed(TicketBotMessages.User.FeedbackReceivedUpdateMessage(ticket));
+		});
+	}
 }
