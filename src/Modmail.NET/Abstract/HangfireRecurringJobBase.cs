7ddcdd4e16fd9e63ac13bd7b31112f32f4fe1ec5
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-17 20:22:06 +0300

    feat(HangfireRecurringJobBase.cs): implement trigger job method

--- src/Modmail.NET/Abstract/HangfireRecurringJobBase.cs
+++ src/Modmail.NET/Abstract/HangfireRecurringJobBase.cs
@@ -5,22 +5,26 @@ namespace Modmail.NET.Abstract;
 
 public abstract class HangfireRecurringJobBase : IRecurringJobDefinition
 {
-  protected HangfireRecurringJobBase(string id, string cronExpression) {
-    Id = id;
-    CronExpression = cronExpression;
-  }
+	protected HangfireRecurringJobBase(string id, string cronExpression) {
+		Id = id;
+		CronExpression = cronExpression;
+	}
 
-  protected string CronExpression { get; }
-  protected string Id { get; }
+	protected string CronExpression { get; }
+	protected string Id { get; }
 
-  public virtual void RegisterRecurringJob(IRecurringJobManager recurringJobManager) {
-    Log.Information("Registering Recurring Job {Id}", Id);
-    recurringJobManager.AddOrUpdate(
-                                    Id,
-                                    () => Execute(),
-                                    CronExpression
-                                   );
-  }
+	public virtual void RegisterRecurringJob(IRecurringJobManager recurringJobManager) {
+		Log.Information("[RECURRING JOB] Registering {Id}", Id);
+		recurringJobManager.AddOrUpdate(
+		                                Id,
+		                                () => Execute(),
+		                                CronExpression
+		                               );
+	}
 
-  public abstract Task Execute();
+	public abstract Task Execute();
+
+	public void TriggerJob() {
+		RecurringJob.TriggerJob(Id);
+	}
 }
