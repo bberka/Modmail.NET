e3337c328774f893fafefddad28dbcbd9c267028
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-17 18:51:38 +0300

    feat: implement UserTeamInformation caching

--- src/Modmail.NET/Features/Teams/Queries/GetUserTeamInformationQuery.cs
+++ src/Modmail.NET/Features/Teams/Queries/GetUserTeamInformationQuery.cs
@@ -1,5 +1,7 @@
+using Modmail.NET.Attributes;
 using Modmail.NET.Features.Teams.Models;
 
 namespace Modmail.NET.Features.Teams.Queries;
 
+[CachePolicy("GetUserTeamInformationQuery", 5)]
 public sealed record GetUserTeamInformationQuery : IRequest<UserTeamInformation[]>;
