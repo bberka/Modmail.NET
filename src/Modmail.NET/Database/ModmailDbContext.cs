dbf07e794b47887358544f0655e91d0161432832
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-19 14:40:04 +0300

    feat(ModmailDbContext.cs): implement field, embed entity properties

--- src/Modmail.NET/Database/ModmailDbContext.cs
+++ src/Modmail.NET/Database/ModmailDbContext.cs
@@ -6,28 +6,30 @@ namespace Modmail.NET.Database;
 
 public class ModmailDbContext : DbContext
 {
-  public ModmailDbContext() { }
-  public ModmailDbContext(DbContextOptions<ModmailDbContext> options) : base(options) { }
-  public required DbSet<Ticket> Tickets { get; set; }
-  public required DbSet<TicketMessageAttachment> MessageAttachments { get; set; }
-  public required DbSet<TicketMessage> Messages { get; set; }
-  public required DbSet<TicketMessageHistory> MessageHistory { get; set; }
-  public required DbSet<Option> Options { get; set; }
-  public required DbSet<Team> Teams { get; set; }
-  public required DbSet<TeamUser> TeamUsers { get; set; }
-  public required DbSet<TeamPermission> TeamPermissions { get; set; }
-  public required DbSet<TicketNote> TicketNotes { get; set; }
-  public required DbSet<UserInformation> UserInformation { get; set; }
-  public required DbSet<Blacklist> Blacklists { get; set; }
-  public required DbSet<TicketType> TicketTypes { get; set; }
-  public required DbSet<Statistic> Statistics { get; set; }
-  public required DbSet<Tag> Tags { get; set; }
+	public ModmailDbContext() { }
+	public ModmailDbContext(DbContextOptions<ModmailDbContext> options) : base(options) { }
+	public required DbSet<Ticket> Tickets { get; set; }
+	public required DbSet<TicketMessageAttachment> MessageAttachments { get; set; }
+	public required DbSet<TicketMessage> Messages { get; set; }
+	public required DbSet<TicketMessageHistory> MessageHistory { get; set; }
+	public required DbSet<Option> Options { get; set; }
+	public required DbSet<Team> Teams { get; set; }
+	public required DbSet<TeamUser> TeamUsers { get; set; }
+	public required DbSet<TeamPermission> TeamPermissions { get; set; }
+	public required DbSet<TicketNote> TicketNotes { get; set; }
+	public required DbSet<UserInformation> UserInformation { get; set; }
+	public required DbSet<Blacklist> Blacklists { get; set; }
+	public required DbSet<TicketType> TicketTypes { get; set; }
+	public required DbSet<Statistic> Statistics { get; set; }
+	public required DbSet<Tag> Tags { get; set; }
+	public required DbSet<Field> Fields { get; set; }
+	public required DbSet<Embed> Embeds { get; set; }
 
-  protected override void OnModelCreating(ModelBuilder modelBuilder) {
-    modelBuilder.ApplyConfigurationsFromAssembly(typeof(ModmailDbContext).Assembly);
-  }
+	protected override void OnModelCreating(ModelBuilder modelBuilder) {
+		modelBuilder.ApplyConfigurationsFromAssembly(typeof(ModmailDbContext).Assembly);
+	}
 
-  protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder) {
-    configurationBuilder.ConfigureSmartEnum();
-  }
+	protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder) {
+		configurationBuilder.ConfigureSmartEnum();
+	}
 }
