6096505473b585df0093755929baceaa91055aed
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-19 14:46:01 +0300

    feat(DbLength.cs): implement discord color constant, merged messages

--- src/Modmail.NET/Common/Static/DbLength.cs
+++ src/Modmail.NET/Common/Static/DbLength.cs
@@ -2,20 +2,19 @@
 
 public static class DbLength
 {
-  public const int Name = 128;
-  public const int Url = 4000;
-  public const int Email = 128;
-  public const int Locale = 10;
-  public const int BotMessage = 1000; //Greetings, closing messages
-  public const int Message = int.MaxValue;
-  public const int Reason = 300;
-  public const int FeedbackMessage = 1000;
-  public const int FileName = 500;
-  public const int MediaType = 100;
-  public const int Note = 1000;
-  public const int KeyString = 128;
-  public const int Emoji = 100;
-  public const int Description = 1000;
-  public const int TagName = 32;
-  public const int TagTitle = 64;
+	public const int Name = 128;
+	public const int Url = 4000;
+	public const int Email = 128;
+	public const int Locale = 10;
+	public const int Message = 4096;
+	public const int Reason = 300;
+	public const int FileName = 500;
+	public const int MediaType = 100;
+	public const int Note = 1000;
+	public const int KeyString = 128;
+	public const int Emoji = 100;
+	public const int Description = 1000;
+	public const int TagName = 32;
+	public const int Title = 64;
+	public const int DiscordColor = 6;
 }
