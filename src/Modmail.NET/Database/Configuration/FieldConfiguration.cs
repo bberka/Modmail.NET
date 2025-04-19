e9726618a3c0a47789fbd95dafdd3138e8994004
Berkay <77205615+bberka@users.noreply.github.com>
2025-04-19 14:39:11 +0300

    feat: field entity

---
+++ src/Modmail.NET/Database/Configuration/FieldConfiguration.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Modmail.NET.Database.Entities;
+
+namespace Modmail.NET.Database.Configuration;
+
+public class FieldConfiguration : IEntityTypeConfiguration<Field>
+{
+	public void Configure(EntityTypeBuilder<Field> builder) {
+		builder.HasKey(x => x.Id);
+	}
+}
