48c59f6b7468b0214ff80dc5065c7b8aed1ed7b2
Berkay <berkaybayarg@gmail.com>
2026-02-05 13:41:43 +0300

    refactor: restructure `BotConfig` to align with hierarchical configuration keys

--- src/Modmail.NET/BotConfig.cs
+++ src/Modmail.NET/BotConfig.cs
@@ -1,83 +1,43 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Serilog.Events;
 
 namespace Modmail.NET;
 
 public class BotConfig
 {
-    private static BotConfig? _instance;
-    private readonly IConfigurationRoot _configManager;
-
     private BotConfig()
     {
-        _configManager = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true)
+        var configManager = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true)
             .AddJsonFile("appsettings.Development.json", true, true)
             .Build();
-        Environment = Enum.Parse<EnvironmentType>(_configManager["ENVIRONMENT"] ?? "Development", true);
-        BotToken = _configManager["BOT_TOKEN"] ?? throw new Exception("BOT_TOKEN is not set.");
-        BotClientId = _configManager["BOT_CLIENT_ID"] ?? throw new Exception("BOT_CLIENT_ID is not set.");
-        BotClientSecret = _configManager["BOT_CLIENT_SECRET"] ?? throw new Exception("BOT_CLIENT_SECRET is not set.");
-        BotPrefix = _configManager["BOT_PREFIX"] ?? "!!";
-        MainServerId = ulong.Parse(_configManager["MAIN_SERVER_ID"] ?? "0");
-        OwnerUsers = _configManager["OWNER_USERS"]
+
+        Environment = Enum.Parse<EnvironmentType>(configManager["Bot:Environment"] ?? "Development", true);
+        BotToken = configManager["Bot:BotToken"] ?? throw new Exception("Bot:BotToken is not set.");
+        BotClientId = configManager["Bot:BotClientId"] ?? throw new Exception("Bot:BotClientId is not set.");
+        BotClientSecret = configManager["Bot:BotClientSecret"] ?? throw new Exception("Bot:BotClientSecret is not set.");
+        BotPrefix = configManager["Bot:BotPrefix"] ?? "!!";
+        MainServerId = ulong.Parse(configManager["Bot:MainServerId"] ?? "0");
+        SuperUsers = configManager["Bot:SuperUsers"]
             ?.Split(',')
             .Select(ulong.Parse)
-            .ToArray() ?? Array.Empty<ulong>();
-        DbConnectionString = _configManager["DB_CONNECTION_STRING"] ?? throw new Exception("DB_CONNECTION_STRING is not set.");
-        LogLevel = Enum.Parse<LogEventLevel>(_configManager["LOG_LEVEL"] ?? "Information", true);
-        LogSinkToFile = bool.Parse(_configManager["LOG_SINK_TO_FILE"] ?? "true");
-        LogSinkToConsole = bool.Parse(_configManager["LOG_SINK_TO_CONSOLE"] ?? "true");
-        EncryptionKey = _configManager["ENCRYPTION_KEY"] ?? throw new Exception("ENCRYPTION_KEY is not set.");
-        DefaultLanguage = _configManager["DEFAULT_LANGUAGE"] ?? "en";
+            .ToArray() ?? [];
+        DbConnectionString = configManager["Bot:DbConnectionString"] ?? throw new Exception("Bot:DbConnectionString is not set.");
     }
-
 
     public static BotConfig This
     {
         get
         {
-            _instance ??= new BotConfig();
-            return _instance;
+            field ??= new BotConfig();
+            return field;
         }
     }
 
-    //Name: ENVIRONMENT
     public EnvironmentType Environment { get; set; }
-
-    //Name: BOT_TOKEN
     public string BotToken { get; set; }
-
-    //Name: BOT_CLIENT_ID
     public string BotClientId { get; set; }
-
-    //Name: BOT_CLIENT_SECRET
     public string BotClientSecret { get; set; }
-
-    //Name: BOT_PREFIX
     public string BotPrefix { get; set; }
-
-    //Name: MAIN_SERVER_ID
     public ulong MainServerId { get; set; }
-
-    //Name: OWNER_USERS
-    public ulong[] OwnerUsers { get; set; }
-
-
-    //Name: DB_CONNECTION_STRING
+    public ulong[] SuperUsers { get; set; }
     public string DbConnectionString { get; set; }
-
-    //Name: ENCRYPTION_KEY
-    public string EncryptionKey { get; set; }
-
-    //Name:  LOG_LEVEL
-    public LogEventLevel LogLevel { get; set; }
-
-    //Name: LOG_SINK_TO_FILE
-    public bool LogSinkToFile { get; set; }
-
-    //Name: LOG_SINK_TO_CONSOLE
-    public bool LogSinkToConsole { get; set; }
-
-    //Name: DEFAULT_LANGUAGE
-    public string DefaultLanguage { get; set; }
 }
